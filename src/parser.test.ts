e4abdd364ab7be0908ccfc866c5c84e6d4f56b6f
Wycliff Wasonga <74678465+wycliffwasonga@users.noreply.github.com>
2021-02-28 09:38:34 +0300

    add a failing test for a chapter with >1 verses

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -88,6 +88,82 @@ describe('USFM Parser', function() {
 
             assert.deepStrictEqual(result, expected);
         });
+
+        it("should return USFMDocument with a Chapter object, that has 2 Verse objects, as its only child when input is \\c_#\\n\\v_#_text\\n\\v_#_text", function() {
+            const result  = parser.parse('\c 1\n\v 1 This is verse 1 of chapter 1.\n\v 2 This is verse 2 of chapter 1.');
+
+            const expected: USFMDocument = {
+                type: 'root',
+                children: [
+                    {
+                        id: 1,
+                        type: 'chapter',
+                        children: [
+                            {
+                                id: 1,
+                                type: 'verse',
+                                text: 'This is verse 1 of chapter 1.',
+                                position: {
+                                    start: {
+                                        line: 2,
+                                        column: 1,
+                                        offset: 4
+                                    },
+                                    end: {
+                                        line: 2,
+                                        column: 34,
+                                        offset: 37
+                                    }
+                                }
+                            },
+                            {
+                                id: 2,
+                                type: 'verse',
+                                text: 'This is verse 2 of chapter 1.',
+                                position: {
+                                    start: {
+                                        line: 3,
+                                        column: 1,
+                                        offset: 38
+                                    },
+                                    end: {
+                                        line: 3,
+                                        column: 34,
+                                        offset: 71
+                                    }
+                                }
+                            }
+                        ],
+                        position: {
+                            start: {
+                                line: 1,
+                                column: 1,
+                                offset: 0
+                            },
+                            end: {
+                                line: 3,
+                                column: 34,
+                                offset: 71
+                            }
+                        }
+                    }
+                ],
+                position: {
+                    start: {
+                        line: 1,
+                        column: 1,
+                        offset: 0
+                    },
+                    end: {
+                        line: 3,
+                        column: 34,
+                        offset: 71
+                    }
+                }
+            };
+
+            assert.deepStrictEqual(result, expected);
+        });
     });
 
 });
